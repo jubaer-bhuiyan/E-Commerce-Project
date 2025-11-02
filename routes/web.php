5da0405784fadbc0cdccc83c6a5cc9161a11f368
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-02 20:04:09 +0600

    Admin Login Page

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');  
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard.index');
