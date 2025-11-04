a992f705eee07bad142035e7f753f8c9b96a8ebe
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-04 13:52:27 +0600

    User Dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('frontend.dashboard.main.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/admin/dashboard', function () {
