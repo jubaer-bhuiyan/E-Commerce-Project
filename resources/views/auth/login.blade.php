4f9b64aef35bedbbdf0658a36381ae85dae8af2b
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-04 11:50:21 +0600

    Master Home app

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -45,3 +45,64 @@
         </div>
     </form>
 </x-guest-layout>
+ --}}
+
+ @extends('frontend.layouts.app')
+
+ @section('contents')
+        <x-frontend.breadcrumb :items="[
+            ['label' => 'Home', 'url' => '/'],
+            ['label' => 'Login']
+        ]" />
+        <div class="page-content pt-150 pb-135">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
+                        <div class="row">
+                            <div class="col-lg-6 pr-30 d-none d-lg-block">
+                                <img class="border-radius-15" src="assets/imgs/page/login-1.png" alt="" />
+                            </div>
+                            <div class="col-lg-6 col-md-8">
+                                <div class="login_wrap widget-taber-content background-white">
+                                    <div class="padding_eight_all bg-white">
+                                        <div class="heading_s1">
+                                            <h1 class="mb-5">Login</h1>
+                                            <p class="mb-30">Don't have an account? <a href="page-register.html">Create
+                                                    here</a></p>
+                                        </div>
+                                        <form method="post">
+                                            <div class="form-group">
+                                                <input type="text" required="" name="email"
+                                                    placeholder="Username or Email *" />
+                                            </div>
+                                            <div class="form-group">
+                                                <input required="" type="password" name="password"
+                                                    placeholder="Your password *" />
+                                            </div>
+                                            
+                                            <div class="login_footer form-group mb-50">
+                                                <div class="chek-form">
+                                                    <div class="custome-checkbox">
+                                                        <input class="form-check-input" type="checkbox" name="checkbox"
+                                                            id="exampleCheckbox1" value="" />
+                                                        <label class="form-check-label"
+                                                            for="exampleCheckbox1"><span>Remember me</span></label>
+                                                    </div>
+                                                </div>
+                                                <a class="text-muted" href="#">Forgot password?</a>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-heading btn-block hover-up"
+                                                    name="login">Log in</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+ @endsection
