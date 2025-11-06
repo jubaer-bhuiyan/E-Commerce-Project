fcd3a4f92eaa59bd1547c14bceed70ca1b6a3f1e
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-06 12:29:54 +0600

    Vendor Login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\Frontend\ProfileController;
+use App\Http\Controllers\Frontend\VendorDashboardController;
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,11 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
     Route::put('/profile', [ProfileController::class, 'profileUpdate'])->name('profile.update');
     Route::put('/profile/password', [ProfileController::class, 'passwordUpdate'])->name('password.update');
+});
+
+// Vendor routes
+Route::group(['prefix' => 'vendor', 'as' => 'vendor.', 'middleware' => ['auth', 'verified']], function () {
+    Route::get('/dashboard', [VendorDashboardController::class, 'index'])->name('dashboard');
 });
 
 require __DIR__ . '/auth.php';
