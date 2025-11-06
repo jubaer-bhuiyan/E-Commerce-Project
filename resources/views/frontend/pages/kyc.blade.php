92d3cae32b9daa622463042cbf494384f2f8aabf
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-06 13:22:43 +0600

    Kyc Verification Form

--- resources/views/frontend/pages/kyc.blade.php
+++ resources/views/frontend/pages/kyc.blade.php
@@ -1,53 +1,73 @@
 @extends('frontend.layouts.app')
 
 @section('contents')
-    <x-frontend.breadcrumb :items="[
-            ['label' => 'Home', 'url' => '/'],
-            ['label' => 'Login']
-        ]" />
-        <div class="page-content pt-150 pb-135">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
-                        <div class="row">
-                            <div class="col-lg-6 col-md-8 offset-lg-3">
-                                <!-- Session Status -->
-                                <x-auth-session-status class="mb-4" :status="session('status')" />
-                                <div class="login_wrap widget-taber-content background-white">
-                                    <div class="padding_eight_all bg-white">
-                                        <div class="heading_s1 mb-4">
-                                            <h4 class="mb-5">Kyc Verification</h4>
-                                        </div>
-                                        <form method="POST" action="{{ route('login') }}">
-                                            @csrf
-                                            <div class="form-group">
-                                                <input type="email" required="" name="email"
-                                                    placeholder="Your Email *" value="{{ old('email') }}"/>
-                                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                                            </div>
-                                            <div class="form-group">
-                                                <input required="" type="password" name="password"
-                                                    placeholder="Your password *" />
-                                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                                            </div>
-
-                                            <div class="login_footer form-group mb-50">
-                                                <div class="chek-form">
-                                                    <div class="custome-checkbox">
-                                                        <input class="form-check-input" type="checkbox" name="remember"
-                                                            id="exampleCheckbox1" value="" />
-                                                        <label class="form-check-label"
-                                                            for="exampleCheckbox1"><span>Remember me</span></label>
-                                                    </div>
-                                                </div>
-                                                <a class="text-muted" href="{{ route('password.request') }}">Forgot password?</a>
-                                            </div>
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-heading btn-block hover-up"
-                                                    name="login">Log in</button>
-                                            </div>
-                                        </form>
+    <x-frontend.breadcrumb :items="[['label' => 'Home', 'url' => '/'], ['label' => 'Login']]" />
+    <div class="page-content pt-150 pb-135">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-8 offset-lg-3">
+                            <!-- Session Status -->
+                            <x-auth-session-status class="mb-4" :status="session('status')" />
+                            <div class="login_wrap widget-taber-content background-white">
+                                <div class="padding_eight_all bg-white">
+                                    <div class="heading_s1 mb-4">
+                                        <h4 class="mb-5">Kyc Verification</h4>
                                     </div>
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <label for="" class="font-weight-bold">Full Name <span class="text-danger">*</span></label>
+                                            <input type="text" required="" name="full_name" placeholder="" />
+                                            <x-input-error :messages="$errors->get('full_name')" class="mt-2" />
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="" class="font-weight-bold">Date of birth <span class="text-danger">*</span></label>
+                                            <input type="text" required="" name="date_of_birth"
+                                                placeholder="1990/7/9" />
+                                            <x-input-error :messages="$errors->get('date_of_birth')" class="mt-2" />
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="" class="font-weight-bold">Gender <span class="text-danger">*</span></label>
+                                            <select name="gender" id="" class="form-control">
+                                                <option value="">Select</option>
+                                                <option value="male">Male</option>
+                                                <option value="female">Female</option>
+                                            </select>
+                                            <x-input-error :messages="$errors->get('gender')" class="mt-2" />
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="" class="font-weight-bold">Full Address <span class="text-danger">*</span></label>
+                                            <input type="text" required="" name="full_address" placeholder="" />
+                                            <x-input-error :messages="$errors->get('full_address')" class="mt-2" />
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="" class="font-weight-bold">Document Type <span class="text-danger">*</span></label>
+                                            <select name="document_type" id="" class="form-control">
+                                                <option value="">Select</option>
+                                                <option value="id_card">ID Card</option>
+                                                <option value="passport">Passport</option>
+                                                <option value="driving_license">Driving License</option>
+                                            </select>
+                                            <x-input-error :messages="$errors->get('document_type')" class="mt-2" />
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="" class="font-weight-bold">Document Scan Copy <span class="text-danger">*</span></label>
+                                            <input type="file" required="" name="document_scan_copy" />
+                                            <x-input-error :messages="$errors->get('document_scan_copy')" class="mt-2" />
+                                        </div>
+
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-heading btn-block hover-up"
+                                                name="">Submit</button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
@@ -55,4 +75,5 @@
                 </div>
             </div>
         </div>
+    </div>
 @endsection
