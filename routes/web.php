e9aee24e6527f83d9fac970de7171f5c160a0606
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-09 07:53:36 +0600

    Store Profile Created

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Frontend\KycController;
 use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\Frontend\ProfileController;
+use App\Http\Controllers\Frontend\StoreController;
 use App\Http\Controllers\Frontend\VendorDashboardController;
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
@@ -28,6 +29,9 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 // Vendor routes
 Route::group(['prefix' => 'vendor', 'as' => 'vendor.', 'middleware' => ['auth', 'verified', 'role:vendor']], function () {
     Route::get('/dashboard', [VendorDashboardController::class, 'index'])->name('dashboard');
+
+    // Shop Profile routes
+    Route::resource('store-profile', StoreController::class);
 });
 
 require __DIR__ . '/auth.php';
