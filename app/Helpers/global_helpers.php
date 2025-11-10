fd2e83a11d2e32c2847f3e4896aa2d79ad1a8804
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-10 21:06:38 +0600

    Super Admin Role Created

--- app/Helpers/global_helpers.php
+++ app/Helpers/global_helpers.php
@@ -5,6 +5,8 @@
 if (!function_exists('hasPermission')) {
     function hasPermission(array $permissions): bool
     {
+        if (auth('admin')->user()->hasRole('Super Admin')) return true;
+        
         return auth('admin')->user()->hasAnyPermission($permissions);
     }
 }
