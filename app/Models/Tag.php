912135862089454f5fbfec2aa22f930cbe20b4c9
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-17 22:20:07 +0600

    Product Module Attribute Tag Table

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
-    //
+    function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
