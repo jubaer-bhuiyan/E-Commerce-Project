ad77c6ec8b068d9a6c732b109f2df11b908e503c
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-18 08:28:49 +0600

    Product Module Work with Add Attribute Button

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -267,7 +267,7 @@
 
                     <div class="card mt-3" id="product-images">
                         <div class="card-header">
-                            <h3 class="card-title">Product Attributes</h3>
+                            <h3 class="card-title">Product Attributes</h3> 
                         </div>
                         <div class="card-body">
                             <div class="col-md-12">
