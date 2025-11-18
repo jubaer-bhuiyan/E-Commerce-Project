f8acdf1436ff74ae9d816b2115ca88412a67f576
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-18 22:31:54 +0600

    Digital Product Permission Setup

--- database/seeders/Admin/PermissionSeeder.php
+++ database/seeders/Admin/PermissionSeeder.php
@@ -13,11 +13,15 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         $permissions = [
-            ['id' => '1', 'name' => 'KYC Management', 'guard_name' => 'admin', 'group_name' => 'KYC Management'],
-            ['id' => '2', 'name' => 'Role Management', 'guard_name' => 'admin', 'group_name' => 'Access Management'],
-            ['id' => '3', 'name' => 'Role User Management', 'guard_name' => 'admin', 'group_name' => 'Access Management'],
+            array('id' => '1', 'name' => 'KYC Management', 'guard_name' => 'admin', 'group_name' => 'KYC Management'),
+            array('id' => '2', 'name' => 'Role Management', 'guard_name' => 'admin', 'group_name' => 'Access Management'),
+            array('id' => '3', 'name' => 'Role User Management', 'guard_name' => 'admin', 'group_name' => 'Access Management'),
+            array('id' => '5', 'name' => 'Category Management', 'guard_name' => 'admin', 'group_name' => 'Product Categoires'),
+            array('id' => '6', 'name' => 'Tags Management', 'guard_name' => 'admin', 'group_name' => 'Product Tags'),
+            array('id' => '7', 'name' => 'Brand Management', 'guard_name' => 'admin', 'group_name' => 'Product Brands'),
+            array('id' => '8', 'name' => 'Product Management', 'guard_name' => 'admin', 'group_name' => 'Products', )
         ];
-        
+
         DB::table('permissions')->insert($permissions);
     }
 }
