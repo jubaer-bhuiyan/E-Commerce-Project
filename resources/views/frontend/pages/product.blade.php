3002d1dad213ee2a0e7b6eefdc01b379e715fe98
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-20 11:33:05 +0600

    Add to Cart button Refactor

--- resources/views/frontend/pages/product.blade.php
+++ resources/views/frontend/pages/product.blade.php
@@ -109,7 +109,7 @@
                 }
             }
 
-
+ 
             $(document).on('click', '.add_to_cart', function(e) {
                 e.preventDefault();
                 var self = $(this);
@@ -118,7 +118,7 @@
                 const variantId = $(this).attr('data-variant');
                 const modal = $(this).data('modal');
 
- 
+
                 $.ajax({
                     url: "{{ route('cart.add') }}",
                     method: "POST",
