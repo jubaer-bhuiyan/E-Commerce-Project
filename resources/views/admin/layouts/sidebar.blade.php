99d35328c9be6459f237a6a3177a1343da00e03e
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-24 19:20:47 +0600

    Order Handling

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -289,7 +289,28 @@
                             </div>
                         </div>
                     </li>
-                    @endif
+                @endif
+
+                @if (hasPermission(['Category Management', 'Tags Management', 'Brand Management']))
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
+                            data-bs-auto-close="false" role="button" aria-expanded="false">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <i class="ti ti-shield"></i>
+                            </span>
+                            <span class="nav-link-title"> Orders </span>
+                        </a>
+                        <div class="dropdown-menu">
+                            <div class="dropdown-menu-columns">
+                                    <div class="dropdown-menu-column">
+                                        <a class="dropdown-item" href="{{ route('admin.orders.index') }}">
+                                            All Orders
+                                        </a>
+                                    </div>
+                            </div>
+                        </div>
+                    </li>
+                @endif
 
                     @if (hasPermission(['KYC Management']))
                         <li class="nav-item dropdown">
