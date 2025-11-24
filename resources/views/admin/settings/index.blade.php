2b0694497ea1ce291107c13699bc56c9936d4042
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-24 12:12:18 +0600

    Payment Gateway

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -8,13 +8,17 @@
                     <div class="card-body">
                         <h4 class="subheader">Business settings</h4>
                         <div class="list-group list-group-transparent">
-                            <a href="./settings.html"
+                            <a href="{{ route('admin.settings.index') }}"
                                 class="list-group-item list-group-item-action d-flex align-items-center active">General Settings</a>
+
+                            <a href="{{ route('admin.commission-settings.index') }}"
+                                class="list-group-item list-group-item-action d-flex align-items-center">Commission Settings</a>
+                            <a href="{{ route('admin.logo-settings.index') }}"
+                                class="list-group-item list-group-item-action d-flex align-items-center">Logo Settings</a>
+                            <a href="{{ route('admin.site-settings.index') }}"
+                                class="list-group-item list-group-item-action d-flex align-items-center">Site Settings</a>
                         </div>
-                        <h4 class="subheader mt-4">Experience</h4>
-                        <div class="list-group list-group-transparent">
-                            <a href="#" class="list-group-item list-group-item-action">Give Feedback</a>
-                        </div>
+
                     </div>
                 </div>
                 <div class="col-12 col-md-9 d-flex flex-column">
