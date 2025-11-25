145b142762b918c17c57dbc54af96f914cdf83b5
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-25 22:28:49 +0600

    Hero Section

--- resources/views/frontend/home/sections/category-section.blade.php
+++ resources/views/frontend/home/sections/category-section.blade.php
@@ -9,84 +9,18 @@
         </div>
         <div class="carausel-10-columns-cover position-relative">
             <div class="carausel-10-columns" id="carausel-10-columns">
+                @foreach ($featuredCategories as $category)
                 <div class="card-2 bg-9 wow animate__animated animate__fadeInUp" data-wow-delay=".1s">
                     <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-13.png" alt="" /></a>
+                        <a href="{{ route('products.index', ['category' => $category->slug]) }}"><img src="{{ asset($category->image) }}" alt="" /></a>
                     </figure>
-                    <h6><a href="shop-grid-right.html">Sunglasses</a></h6>
-                    <span>26 items</span>
+                    <h6><a href="{{ route('products.index', ['category' => $category->slug]) }}">{{ $category->name }}</a></h6>
+                    <span>{{ $category->products_count }} items</span>
                 </div>
-                <div class="card-2 bg-10 wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
-                    <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-12.png" alt="" /></a>
-                    </figure>
-                    <h6><a href="shop-grid-right.html">Whatches</a></h6>
-                    <span>28 items</span>
-                </div>
-                <div class="card-2 bg-11 wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
-                    <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-11.png" alt="" /></a>
-                    </figure>
-                    <h6><a href="shop-grid-right.html">LED TV</a></h6>
-                    <span>14 items</span>
-                </div>
-                <div class="card-2 bg-12 wow animate__animated animate__fadeInUp" data-wow-delay=".4s">
-                    <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-9.png" alt="" /></a>
-                    </figure>
-                    <h6><a href="shop-grid-right.html">Shoes</a></h6>
-                    <span>54 items</span>
-                </div>
-                <div class="card-2 bg-13 wow animate__animated animate__fadeInUp" data-wow-delay=".5s">
-                    <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-3.png" alt="" /></a>
-                    </figure>
-                    <h6><a href="shop-grid-right.html">Cosmetics</a></h6>
-                    <span>56 items</span>
-                </div>
-                <div class="card-2 bg-14 wow animate__animated animate__fadeInUp" data-wow-delay=".6s">
-                    <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-1.png" alt="" /></a>
-                    </figure>
-                    <h6><a href="shop-grid-right.html">Winter Wear</a></h6>
-                    <span>72 items</span>
-                </div>
-                <div class="card-2 bg-15 wow animate__animated animate__fadeInUp" data-wow-delay=".7s">
-                    <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-2.png" alt="" /></a>
-                    </figure>
-                    <h6><a href="shop-grid-right.html">Kid's</a></h6>
-                    <span>36 items</span>
-                </div>
-                <div class="card-2 bg-12 wow animate__animated animate__fadeInUp" data-wow-delay=".8s">
-                    <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-4.png" alt="" /></a>
-                    </figure>
-                    <h6><a href="shop-grid-right.html">Headphones</a></h6>
-                    <span>123 items</span>
-                </div>
-                <div class="card-2 bg-10 wow animate__animated animate__fadeInUp" data-wow-delay=".9s">
-                    <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-5.png" alt="" /></a>
-                    </figure>
-                    <h6><a href="shop-grid-right.html">Jewelry</a></h6>
-                    <span>34 items</span>
-                </div>
-                <div class="card-2 bg-12 wow animate__animated animate__fadeInUp" data-wow-delay="1s">
-                    <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-14.png" alt="" /></a>
-                    </figure>
-                    <h6><a href="shop-grid-right.html">DSLR Camera</a></h6>
-                    <span>89 items</span>
-                </div>
-                <div class="card-2 bg-10 wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
-                    <figure class="img-hover-scale overflow-hidden">
-                        <a href="shop-grid-right.html"><img src="assets/imgs/shop/cat-12.png" alt="" /></a>
-                    </figure>
-                    <h6><a href="shop-grid-right.html">Whatches</a></h6>
-                    <span>28 items</span>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
 </section>
+
+// No Change
