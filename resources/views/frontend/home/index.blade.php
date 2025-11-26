5b3770640e6fd401e0754fba403bd1124ea14325
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-11-26 08:09:51 +0600

    Product Section

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -1,49 +1,31 @@
 @extends('frontend.layouts.app')
 
 @section('contents')
-    <!--Start hero slider-->
     @include('frontend.home.sections.hero-section')
     <!--End hero slider-->
-
-    <!--Start category slider-->
     @include('frontend.home.sections.category-section')
     <!--End category slider-->
-
-    <!--Start banners-->
     @include('frontend.home.sections.banner-section')
     <!--End banners-->
-
-    <!--Start products tabs-->
-    @include('frontend.home.sections.products-tabs-section')
-    <!--End Products Tabs-->
-
-    <!--Start 4 banners(Banner Section Two)-->
+    @include('frontend.home.sections.products-tab-section')
+    <!--Products Tabs-->
     @include('frontend.home.sections.banner-section-two')
-    <!--End 4 banners(Banner Section Two)-->
-
-    <!--Start Daily Best Sells(flash-sale-section)-->
+    <!--End 4 banners-->
     @include('frontend.home.sections.flash-sale-section')
-    <!--End Best Sales(flash-sale-section)-->
-
-    <!-- new arrival start -->
+    <!--End Best Sales-->
     @include('frontend.home.sections.new-arrival-section')
     <!-- new arrival end -->
-
-    <!--CTA section start-->
     <section class="wsus__ctg mt-40">
         <div class="container">
-            <a href="#" class="wsus__ctg_area">
-                <img src="assets/imgs/cta_bg.png" alt="cta" class="img-fluid w-100" />
+            <a href="{{ data_get($ads, 'side_banner_two.0.url', '') }}" class="wsus__ctg_area">
+                <img src="{{ asset(data_get($ads, 'side_banner_two.0.image', '')) }}" alt="cta" class="img-fluid w-100" />
             </a>
         </div>
     </section>
-    <!--CTA section end-->
 
-    <!-- special products start -->
-    @include('frontend.home.sections.special-products-section')
     <!-- special products end -->
-
-    <!--Start 4 columns-->
     @include('frontend.home.sections.four-col-products-section')
     <!--End 4 columns-->
 @endsection
+
+// Complete code
